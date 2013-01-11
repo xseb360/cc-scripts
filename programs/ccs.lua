d075b0d1b29abea2484caa53a09d4e3b06d0aa45
XSeb360 <xseb360@gmail.com>
2013-01-11 15:47:16 -0500

    added test bench

--- programs/ccs.lua
+++ programs/ccs.lua
@@ -22,6 +22,7 @@ programs = {
   "room",
   "shaft",
   "startup",
+  "test",
   "treefarm"
 }
 
---
+++ programs/test.lua
@@ -0,0 +1,31 @@
+
+
+
+function install(path)
+  local installPath = "/cc-scripts/" .. path
+  local updated = fs.exists(installPath)
+
+  print("Downloading " .. path .. " ...")
+  local conn = http.get("http://casper3.dyndns.org:8008/cctest.txt")
+  local file = fs.open(installPath, "w")
+
+  assert(conn, "Unable to download " .. path .. " - aborting!")
+  assert(file, "Unable to save " .. path .. " to " .. installPath .. " - aborting!")
+
+  file.write(conn.readAll())
+
+  file.close()
+  conn.close()
+
+  if updated then
+    print("Updated " .. path)
+  else
+    print("Installed " .. path)
+  end
+end
+
+
+install("apis/cctest")
+
+os.loadAPI("apis/cctest")
+cctest.test()
