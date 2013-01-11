355b2719b52f09d752fd0d710eecd92193fba3ea
XSeb360 <xseb360@gmail.com>
2013-01-11 16:08:37 -0500

    changed test file ext

--- programs/test.lua
+++ programs/test.lua
@@ -6,7 +6,7 @@ function install(path)
   local updated = fs.exists(installPath)
 
   print("Downloading " .. path .. " ...")
-  local conn = http.get("http://casper3.dyndns.org:8008/cctest.txt")
+  local conn = http.get("http://casper3.dyndns.org:8008/cctest.lua")
   local file = fs.open(installPath, "w")
 
   assert(conn, "Unable to download " .. path .. " - aborting!")
