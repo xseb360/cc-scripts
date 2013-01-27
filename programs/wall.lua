5f57254d766f5cd7e31c6e40b460a127794d2405
XSeb360 <xseb360@gmail.com>
2013-01-27 00:47:24 -0500

    changed right for turnRight. removed some help (too verbose).

--- programs/wall.lua
+++ programs/wall.lua
@@ -1,28 +1,20 @@
+os.loadAPI('cc-scripts/apis/inv')
+
+
 local tArgs = { ... }
 
 if #tArgs ~= 2 then
   print("Usage: wall <length> <heigth>")
-  print("  Example: wall 20 4")
-  print("  Notes: * Wall include turtle starting space.")
-  print("         * Wall will be built in the same direction as the turtle.")
-  print("         * Turtle is at the bottom of the wall.")
-  print("         * At least 1 representative building block must be in slot 1.")
-  print("         * All slots may contain building blocks too.")
-  print("         * Will not return to empty itself.")
-  print("         * Will stop in its track waiting for block when empty.")
+  print("Example: wall 20 4")
+  print("* Includes starting space.")
+  print("* Turtle is at the bottom.")
+  print("* Representative building block in slot 1.")
   return
 end
 
 local length = tonumber( tArgs[1] )
 local heigth  = tonumber( tArgs[2] )
 
-
-
-if not turtle.position then
-  os.loadAPI('cc-scripts/apis/betterapi.lua')
-  --  os.loadAPI('turtletracker.lua')
-  os.loadAPI('inv.lua')
-end
 
 
 -- Build 1 layer of the wall.
@@ -48,6 +40,7 @@ for i = 1, heigth do
   buildLayer()
 
   -- turn around
-  turtle.right()
-  turtle.right()
+  turtle.turnRight()
+  turtle.turnRight()
+  
 end
