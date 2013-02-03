c4d57de10cfc9d46eac4b2823fe9e890aed90165
XSeb360 <xseb360@gmail.com>
2013-02-03 04:18:13 -0500

    added space escaping

--- apis/ccstatus.lua
+++ apis/ccstatus.lua
@@ -2,6 +2,8 @@
 
 function report(s)
 
+  s = string.gsub(s, " ", "+")
+
   local conn = http.get("http://casper3.dyndns.org:8008/ccstatus/report.aspx?name="..os.getComputerLabel().."&status="..s)
   conn.readAll()
   conn.close()
