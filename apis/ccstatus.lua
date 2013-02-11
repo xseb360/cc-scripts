27ad216d3c3eb2dccbeb84f89f2c46c3cd4962e5
xseb360 <xseb360@gmail.com>
2013-02-10 19:01:03 -0500

    round down fuel

--- apis/ccstatus.lua
+++ apis/ccstatus.lua
@@ -6,7 +6,7 @@ function report(s)
 
   s = string.gsub(s, " ", "+")
 
-  local fuelLevel = (tonumber(turtle.getFuelLevel()) / 1000).."k";
+  local fuelLevel = math.floor(turtle.getFuelLevel() / 1000).."k";
   if tonumber(turtle.getFuelLevel()) < 10000 then fuelLevel = "!"..fuelLevel end
 
   local conn = http.get("http://casper3.dyndns.org:8008/ccstatus/report.aspx?name="..os.getComputerLabel().."&status=("..fuelLevel..")+"..s)
