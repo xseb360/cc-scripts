091b767be3d49ba88e053da86c45f0e634414ebf
xseb360 <xseb360@gmail.com>
2013-02-15 20:20:06 -0500

    Added base code. 1 min test timer.

--- programs/slaughterhousemanager.lua
+++ programs/slaughterhousemanager.lua
@@ -3,6 +3,7 @@ os.loadAPI('cc-scripts/apis/ccstatus')
 
 local tArgs = { ... }
 
+
 function main()
  	parallel.waitForAny(keepSlaugthering, skip)
   ccstatus.report("Slaughtering done.")
@@ -11,7 +12,58 @@ end
 
 function keepSlaugthering()
   ccstatus.report("Slaughtering...")
+ 
+  while 1 do
+    slaugtherOnce()
+    sleep(1 * 60)
+  end
 
+end
+
+function slaugtherOnce()
+  
+  purge()
+  sleep(3)
+  resupply()
+
+end
+
+function purge()
+
+  closeTop()
+  openBottom()
+
+  sleep(5)
+
+  closeBottom()
+
+end
+
+function resupply()
+
+  closeBottom()
+  openTop()
+
+  sleep(5)
+
+  closeTop()
+
+end
+
+function openTop()
+  redstone.setOutput("top", false)
+end
+
+function closeTop
+  redstone.setOutput("top", true)
+end
+
+function openBottom()
+  redstone.setOutput("bottom", false)
+end
+
+function closeBottom()
+  redstone.setOutput("bottom", true)
 end
 
 
@@ -23,9 +75,6 @@ function skip()
 		end
 	end
 end
-
-
-
 
 
 local ok, err = pcall(main)
