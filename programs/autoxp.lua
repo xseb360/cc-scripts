0fada919359adf8cbe189bff018103312dc75ca5
xseb360 <xseb360@gmail.com>
2013-02-22 14:59:47 -0500

    added press space to stop.

--- programs/autoxp.lua
+++ programs/autoxp.lua
@@ -3,28 +3,51 @@
 -- APIs
 os.loadAPI('cc-scripts/apis/ccstatus')
 
-print("Initializing XP Module...")
 
-m = peripheral.wrap("right")
-m.setAutoCollect(true)
-
-print("Getting XP...")
-
-while true do
- sleep(0.2)
-	if redstone.getInput("bottom") or redstone.getInput("top") or redstone.getInput("right") or redstone.getInput("left") then -- On
-		turtle.attack()
-	end
+function main()
   
-	if math.fmod(m.getLevels(), 5) == 0 then
-    reportLevel()
-  end
+ 	parallel.waitForAny(keepXPing, skip)
 
 end
 
+function keepXPing()
+
+  m.setAutoCollect(true)
+  print("Getting XP (press SPACE to stop)...")
+
+  while true do
+   sleep(0.2)
+	  if redstone.getInput("bottom") or redstone.getInput("top") or redstone.getInput("right") or redstone.getInput("left") then -- On
+		  turtle.attack()
+	  end
+  
+	  if math.fmod(m.getLevels(), 5) == 0 then
+      reportLevel()
+    end
+
+  end
+end
+
+function skip()
+	while true do
+		local id, key = os.pullEvent("key")
+		if key == 57 then
+			break
+		end
+	end
+end
 
 function reportLevel()
-
   ccstatus.report("Current Level: "..m.getLevels())
-
 end
+
+
+print("Initializing XP Module...")
+m = peripheral.wrap("right")
+print("XP Module ready.")
+
+local ok, err = pcall(main)
+if not ok then
+  ccstatus.report(err)
+end
+
