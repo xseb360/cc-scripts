69f59fde07cb677b1fcca47b70940e5174bb7764
xseb360 <xseb360@gmail.com>
2013-02-22 14:54:31 -0500

    added level report

--- programs/autoxp.lua
+++ programs/autoxp.lua
@@ -1,12 +1,30 @@
 -- pastebin get 9p2GvMhu autoexec
 
+-- APIs
+os.loadAPI('cc-scripts/apis/ccstatus')
 
-local m = peripheral.wrap("right")
+print("Initializing XP Module...")
+
+m = peripheral.wrap("right")
 m.setAutoCollect(true)
+
+print("Getting XP...")
 
 while true do
  sleep(0.2)
 	if redstone.getInput("bottom") or redstone.getInput("top") or redstone.getInput("right") or redstone.getInput("left") then -- On
 		turtle.attack()
 	end
+  
+	if math.fmod(m.getLevels(), 5) == 0 then
+    reportLevel()
+  end
+
+end
+
+
+function reportLevel()
+
+  ccstatus.report("Current Level: "..m.getLevels())
+
 end
