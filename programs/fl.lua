7b83ea12ce3b3e667350f615050bd17a0c3dc9d4
xseb360 <xseb360@gmail.com>
2013-02-22 19:34:12 -0500

    added get xp level to fl

--- programs/fl.lua
+++ programs/fl.lua
@@ -1,5 +1,20 @@
-if turtle then
-  turtleLabel = os.getComputerLabel()
-  turtleFuel = turtle.getFuelLevel()
-  print(turtleLabel.." fuel level is : "..turtleFuel)
+
+
+local tArgs = { ... }
+
+
+
+if #tArgs == 1 and tArgs[1] == "xp" then
+
+  m = peripheral.wrap("right")
+  print("Current Level: "..m.getLevels())
+
+else
+
+  if turtle then
+    turtleLabel = os.getComputerLabel()
+    turtleFuel = turtle.getFuelLevel()
+    print(turtleLabel.." fuel level is : "..turtleFuel)
+  end
+
 end
