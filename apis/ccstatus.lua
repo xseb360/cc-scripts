206d6836f82fbf34a76519542b2a6d411613531f
xseb360 <xseb360@gmail.com>
2013-02-22 22:09:18 -0500

    renamed var

--- apis/ccstatus.lua
+++ apis/ccstatus.lua
@@ -6,17 +6,19 @@ function report(s)
 
   s = string.gsub(s, " ", "+")
 
+  statusToReport = "none"
+
   if turtle then
     local fuelLevel = math.floor(turtle.getFuelLevel() / 1000).."k";
     
     if tonumber(turtle.getFuelLevel()) < 10000 then fuelLevel = "!"..fuelLevel end
 
-    status = "("..fuelLevel..")+"..s
+    statusToReport = "("..fuelLevel..")+"..s
   else
-    status = s
+    statusToReport = s
   end
 
-  local conn = http.get("http://casper3.dyndns.org:8008/ccstatus/report.aspx?name="..os.getComputerLabel().."&status="..status)
+  local conn = http.get("http://casper3.dyndns.org:8008/ccstatus/report.aspx?name="..os.getComputerLabel().."&status="..statusToReport)
   
   if conn then
     conn.readAll()
