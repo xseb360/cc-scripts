9e6b82e27424881ef34b83929702e4505843c93d
xseb360 <xseb360@gmail.com>
2013-02-22 22:07:07 -0500

    added support for none turtle computer.

--- apis/ccstatus.lua
+++ apis/ccstatus.lua
@@ -6,10 +6,19 @@ function report(s)
 
   s = string.gsub(s, " ", "+")
 
-  local fuelLevel = math.floor(turtle.getFuelLevel() / 1000).."k";
-  if tonumber(turtle.getFuelLevel()) < 10000 then fuelLevel = "!"..fuelLevel end
+  status = ""
 
-  local conn = http.get("http://casper3.dyndns.org:8008/ccstatus/report.aspx?name="..os.getComputerLabel().."&status=("..fuelLevel..")+"..s)
+  if turtle then
+    local fuelLevel = math.floor(turtle.getFuelLevel() / 1000).."k";
+    
+    if tonumber(turtle.getFuelLevel()) < 10000 then fuelLevel = "!"..fuelLevel end
+
+    status = "("..fuelLevel..")+"..s
+  else
+    status = s
+  end
+
+  local conn = http.get("http://casper3.dyndns.org:8008/ccstatus/report.aspx?name="..os.getComputerLabel().."&status="..status)
   
   if conn then
     conn.readAll()
